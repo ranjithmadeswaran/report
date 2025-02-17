e478f97e9a8325789972012393881044e2f35789
Ranjith <ranjith.madeswaran@dreamstechnologies.com>
2025-02-17 19:09:59 +0530

    changes

--- resources/views/admin/includes.blade.php
+++ resources/views/admin/includes.blade.php
@@ -1,4 +1,7 @@
 @push('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.23/jspdf.plugin.autotable.min.js"></script>
 @if (file_exists(public_path('assets/js/report.js')))
     <script src="{{ asset('assets/js/report.js') }}"></script>
 @endif
--- resources/views/provider/includes.blade.php
+++ resources/views/provider/includes.blade.php
@@ -1,4 +1,15 @@
 @push('scripts')
+<!-- Chart JS -->
+<script src="{{ asset('assets/plugins/apexchart/apexcharts.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/apexchart/chart-data.js') }}"></script>
+
+<!-- Daterangepikcer JS -->
+<script src="{{ asset('assets/js/moment.js') }}"></script>
+<script src="{{ asset('assets/plugins/daterangepicker/daterangepicker.js') }}"></script>
+<script src="{{ asset('assets/js/bootstrap-datetimepicker.min.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.23/jspdf.plugin.autotable.min.js"></script>
 @if (file_exists(public_path('front/js/report.js')))
     <script src="{{ asset('front/js/report.js') }}"></script>
 @endif
